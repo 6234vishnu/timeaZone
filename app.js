9c69f2284d88bf9c4ce53d451de92ddcf81adf8b
vishnu anil <vishnuanil216@gmail.com>
2025-06-22 12:00:08 +0530

    port fixed on app.js

--- app.js
+++ app.js
@@ -12,25 +12,33 @@ const morgan = require("morgan");
 const methodOverride = require("method-override");
 const { clearCacheAdmin, clearCacheUser } = require("./middlewares/clearCache");
 const nocache = require("nocache");
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 
 db();
-const PORT =  3000;
-app.use(session({
-  secret: process.env.SECRECT_KEY,
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI, 
-    
+
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use(
+  session({
+    secret: process.env.SECRECT_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
   })
-}));
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use((req, res, next) => {
   res.locals.user = req.user || null;
   next();
 });
+
 app.use((req, res, next) => {
   res.locals.message = req.session.message || null;
   next();
@@ -38,26 +46,17 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/", router);
-app.use((req, res, next) => {
-  res.setHeader("Cache-Control", "no-store");
-  res.setHeader("Pragma", "no-cache");
-  res.setHeader("Expires", "0");
-  next();
-});
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.use(nocache());
-
 app.use(morgan("dev"));
-
-app.use("/", userRouter);
-app.use("/admin", adminRouter);
-
-app.use("/images", express.static(path.join(__dirname, "../public/images")));
 app.use(methodOverride("_method"));
 
 app.use(clearCacheAdmin, clearCacheUser);
+
 
 app.set("view engine", "ejs");
 app.set("views", [
@@ -66,10 +65,19 @@ app.set("views", [
   path.join(__dirname, "views/partials"),
 ]);
 
-app.use("/images", express.static(path.join(__dirname, "public/images")));
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+app.use("/", router);
+app.use("/", userRouter);
+app.use("/admin", adminRouter);
+
+
+app.get("/", (req, res) => {
+  res.render("home"); 
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
